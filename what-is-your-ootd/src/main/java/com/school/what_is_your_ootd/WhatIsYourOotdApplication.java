645283bad436e5eea1f336c659b02dfe1ac037b4
Jaemin Oh <ojm6135@naver.com>
2025-06-01 16:54:23 +0900

    프로젝트 생성.

---
+++ what-is-your-ootd/src/main/java/com/school/what_is_your_ootd/WhatIsYourOotdApplication.java
@@ -0,0 +1,13 @@
+package com.school.what_is_your_ootd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatIsYourOotdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatIsYourOotdApplication.class, args);
+	}
+
+}
---
+++ what-is-your-ootd/src/test/java/com/school/what_is_your_ootd/WhatIsYourOotdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.what_is_your_ootd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatIsYourOotdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
