5fcdf64598fff2a98bd26061371a26654d804678
Jaemin Oh <ojm6135@naver.com>
2025-06-04 23:22:24 +0900

    api key 노출로 인한 수정, key는 재발급
    - build.gradle에 dotenv 의존성 추가
    - DotenvConfig.java 추가
    - sign-up.html 수정
    - UserController.java 수정

--- what-is-your-ootd/src/main/java/com/school/what_is_your_ootd/controller/UserController.java
+++ what-is-your-ootd/src/main/java/com/school/what_is_your_ootd/controller/UserController.java
@@ -2,6 +2,7 @@ package com.school.what_is_your_ootd.controller;
 
 import com.school.what_is_your_ootd.form.UserRegistrationForm;
 import com.school.what_is_your_ootd.service.UserService;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,14 +15,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
+    private final Dotenv dotenv;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, Dotenv dotenv) {
         this.userService = userService;
+        this.dotenv = dotenv;
     }
 
     @GetMapping("/sign-up")
     public String signupPage(Model model) {
+        model.addAttribute("API_KEY", dotenv.get("KAKAO_MAP_API_KEY"));
         model.addAttribute("form", new UserRegistrationForm());
         return "sign-up";
     }
